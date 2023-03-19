7d37b0d4d08185ebdafd648ace2a319b1f96e90e
Davidofski <108184953+Davidofski@users.noreply.github.com>
2023-03-19 13:58:32 +0100

    implemented but not fully tested

--- sandbox2.py
+++ sandbox2.py
@@ -1,22 +1,4 @@
-import pandas as pd
-import os
-from datetime import datetime
-import errormessage as erm
+import sandbox
 
-# get current location and merge with file name
-cD = os.getcwd()
-db_fileName = "DB_PW.csv"
-db_path = cD + r"\%s"%db_fileName
-
-def readFile():
-    print("[FUNCTION]   sub.readFile()")
-
-    try:
-        db_fileExists = os.path.isfile(db_path)        
-        db_loaded = pd.read_csv(db_path, sep=";", index_col=[0])
-        print("[INFO]   Loaded dataframe:\n", db_loaded)
-
-    except ImportError:
-        pass
-
-    return db_loaded
+variable = sandbox.p1.age
+print(variable)
